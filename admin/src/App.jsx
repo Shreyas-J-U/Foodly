1c96885702bc2d2d1617230eea7331a2f6555731
Shreyas-J-U <sshreyasju@gmail.com>
2025-07-14 11:38:03 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,9 @@ import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  // const url = "http://localhost:4000"
+  const url = "https://foodly-backend-kxdq.onrender.com";
+
   return (
     <div>
       <ToastContainer />
